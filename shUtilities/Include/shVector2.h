758f5adc7af6e6497f47c148b87c1a7261a0fe31
MarcoEsparza <ldv20c.mesparza@uartesdigitales.edu.mx>
2024-09-13 12:47:11 -0600

    Vector2 Feedback
    - Vector2 style error solved.

--- shUtilities/Include/shVector2.h
+++ shUtilities/Include/shVector2.h
@@ -2,7 +2,7 @@
 /*
 *  @file    shVector2.h
 *  @author  MarcoEsparza <maeafinn14@gmail.com>
-*  @date    2024/09/12
+*  @date    2024/09/13
 *  @brief   Vector2 with float
 *
 *  Vector2 with float
@@ -30,7 +30,7 @@ namespace shEngineSDK {
  */
 class SH_UTILITY_EXPORT  Vector2
 {
-  public:
+ public:
   /*
   *  Default constructor.
   */
@@ -39,14 +39,15 @@ class SH_UTILITY_EXPORT  Vector2
   /*
   *  Constructor to initialize values to the given numbers.
   * 
-  *  @param X value, Y value.
+  *  @param float _x: X value.
+  *  @param float _y: Y value.
   */
-  Vector2(float _x, float _y);
+  Vector2(float _x, float _y) : x(_x), y(_y) {}
 
   /*
   *  Copy constructor.
   * 
-  *  @param Vector2.
+  *  @param Vector2 _other: The Vector2 to be copied.
   */
   Vector2(const Vector2& _other);
 
@@ -64,12 +65,12 @@ class SH_UTILITY_EXPORT  Vector2
   /*
   *  Calculates the dot product of two given Vector2s.
   * 
-  *  @param The Vector2 to calculate with this Vector2.
+  *  @param Vector2 _other: To calculate with this Vector2.
   * 
   *  @return float The result of the dot product.
   */
   float
-  dot(const Vector2& other) const;
+  dot(const Vector2& _other) const;
 
   /*
   *  Calculates the magnitude of the Vector2.
@@ -94,39 +95,43 @@ class SH_UTILITY_EXPORT  Vector2
   /*
   *  Calculates the cross product of the Vector2.
   *
-  *  @param The Vector2 to calculate with this Vector2.
+  *  @param Vector2 _other: To calculate with this Vector2.
   *
   *  @return Vector2 The ortogonal Vector2 between this and the other Vector2.
   */
   float
-  scalarProjection(const Vector2& other) const;
+  scalarProjection(const Vector2& _other) const;
 
   /*
   *  Calculates the of a Vector2 to another Vector2.
   *
-  *  @param The first Vector2, the second Vector2 to calculate over the first
+  *  @param Vector2 _other: To calculate over this.
   *
   *  @return Vector2 The ortogonal Vector2 between this and the other Vector2.
   */
   Vector2
-  vectorProjection(const Vector2& other) const;
+  vectorProjection(const Vector2& _other) const;
 
   /*
   *  Calculates the linear interpolate between two Vector2.
   *
-  *  @param Min value, max value, time.
+  *  @param Vector2 _vec: Min value.
+  * 
+  *  @param Vector2 _other: Max value.
+  * 
+  *  @param float _time: Time.
   *
   *  @return Vector2 The result of the linear interpolation.
   */
   Vector2
-  lerp(const Vector2& vec, const Vector2& other, const float time) const;
+  lerp(const Vector2& _vec, const Vector2& _other, const float& _time) const;
 
   /*************************************************************/
   /*
   *  Variables
   */
   /*************************************************************/
-public:
+ public:
   /*
   * Vector2 X value.
   */
@@ -141,70 +146,109 @@ public:
   *  Operator overload
   */
   /*************************************************************/
-public:
+ public:
   /*
-  *  Operator to sum a Vector2 values and other Vector2 values
+  *  Operator to sum a Vector2 values and other Vector2 values.
   *  
-  *  @param lValue-Vector2, rValue-Vector2
+  *  @param lValue-Vector2.
+  * 
+  *  @param rValue-Vector2.
   * 
   *  @return Vector2
   */
   FORCEINLINE Vector2
-  operator+(const Vector2& other) const
-  {
-    return Vector2(x + other.x, y + other.y);
-  }
+  operator+(const Vector2& other) const;
 
   /*
-  *  Operator to substract a Vector2 values and other Vector2 values
+  *  Operator to substract a Vector2 values and other Vector2 values.
   *
-  *  @param lValue-Vector2, rValue-Vector2
+  *  @param lValue-Vector2.
+  * 
+  *  @param rValue-Vector2.
   *
   *  @return Vector2
   */
   FORCEINLINE Vector2
-  operator-(const Vector2& other) const
-  {
-    return Vector2(x - other.x, y - other.y);
-  }
+  operator-(const Vector2& other) const;
 
   /*
-  *  Operator to multiply the values of a Vector2 with a float
+  *  Operator to multiply the values of a Vector2 with a float.
   *
-  *  @param lValue-Vector2, rValue-float
+  *  @param lValue-Vector2.
+  * 
+  *  @param rValue-float.
   *
-  *  @return Vector2 The result of the multiplication
+  *  @return Vector2 The result of the multiplication.
   */
   FORCEINLINE Vector2
-  operator*(const float& delta) const
-  {
-    return Vector2(x * delta, y * delta);
-  }
+  operator*(const float& delta) const;
 
   /*
   *  Operator to sum a Vector2 values and other Vector2 values and store the
-  *  result in the first Vector2
+  *  result in the first Vector2.
   *
-  *  @param lValue-Vector2, rValue-Vector2
+  *  @param lValue-Vector2.
+  * 
+  *  @param rValue-Vector2.
+  * 
+  *  @return Vector2 This
   */
-  FORCEINLINE void
-  operator+=(const Vector2& other)
-  {
-    x += other.x;
-    y += other.y;
-  }
+  FORCEINLINE Vector2
+  operator+=(const Vector2& other);
 
   /*
   *  Operator to substract a Vector2 values and other Vector2 values and store
-  *  the result in the first Vector2
+  *  the result in the first Vector2.
   *
-  *  @param lValue-Vector2, rValue-Vector2
+  *  @param lValue-Vector2.
+  * 
+  *  @param rValue-Vector2.
+  * 
+  *   @return Vector2 This
   */
-  FORCEINLINE void
-  operator-=(const Vector2& other)
-  {
-    x -= other.x;
-    y -= other.y;
-  }
+  FORCEINLINE Vector2
+  operator-=(const Vector2& other);
 };
+
+/*************************************************************/
+/*
+*  Implementations
+*/
+/*************************************************************/
+
+FORCEINLINE Vector2
+Vector2::operator+(const Vector2& other) const
+{
+  return Vector2(x + other.x, y + other.y);
+}
+
+FORCEINLINE Vector2
+Vector2::operator-(const Vector2& other) const
+{
+  return Vector2(x - other.x, y - other.y);
+}
+
+FORCEINLINE Vector2
+Vector2::operator*(const float& delta) const
+{
+  return Vector2(x * delta, y * delta);
+}
+
+FORCEINLINE Vector2
+Vector2::operator+=(const Vector2& other)
+{
+  x += other.x;
+  y += other.y;
+
+  return *this;
+}
+
+FORCEINLINE Vector2
+Vector2::operator-=(const Vector2& other)
+{
+  x -= other.x;
+  y -= other.y;
+
+  return *this;
+}
 }
