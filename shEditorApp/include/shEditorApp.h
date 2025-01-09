995401105ce1a0fec9e8e41168fcc4fc489e5e9e
MarcoEsparza <ldv20c.mesparza@uartesdigitales.edu.mx>
2025-01-09 15:28:58 -0600

    Editor Base App (start up)

---
+++ shEditorApp/include/shEditorApp.h
@@ -0,0 +1,30 @@
+/*************************************************************/
+/*
+*  @file    shEditorApp.h
+*  @author  MarcoEsparza <maeafinn14@gmail.com>
+*  @date    2025/01/09
+*  @brief   App for editor.
+*
+*  App for editor.
+*
+*  @bug     No bug known.
+*/
+/*************************************************************/
+#pragma once
+
+/*************************************************************/
+/*
+*  Includes
+*/
+/*************************************************************/
+#include "shPrerequisitesEditor.h"
+#include "shBaseApp.h"
+
+namespace shEngineSDK {
+class EditorApp : public BaseApp
+{
+ public:
+  EditorApp() = default;
+  ~EditorApp() = default;
+};
+}
---
+++ shEditorApp/source/shEditorApp.cpp
@@ -0,0 +1,22 @@
+/*************************************************************/
+/*
+*  @file    shEditorApp.cpp
+*  @author  MarcoEsparza <maeafinn14@gmail.com>
+*  @date    2025/01/09
+*  @brief   App for editor.
+*
+*  App for editor.
+*
+*  @bug     No bug known.
+*/
+/*************************************************************/
+
+/*************************************************************/
+/*
+*  Includes
+*/
+/*************************************************************/
+#include "shEditorApp.h"
+
+namespace shEngineSDK {
+}
